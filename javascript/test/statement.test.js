c5ede8d509ede159bc29619ef5a6710728744987
Yuya Kazama <yuya.kazama@bizreach.co.jp>
2020-09-29 15:19:13 +0900

    とりあえず動いているものを期待値にする

--- javascript/test/statement.test.js
+++ javascript/test/statement.test.js
@@ -4,6 +4,11 @@ const fs=require('fs');
 test('example statement', () => {
     const invoice = JSON.parse(fs.readFileSync('test/invoice.json', 'utf8'));
     const plays = JSON.parse(fs.readFileSync('test/plays.json', 'utf8'));
-    const expected = '';
+    const expected = 'Statement for BigCo\n' +
+        ' Hamlet: $650.00 (55 seats)\n' +
+        ' As You Like It: $580.00 (35 seats)\n' +
+        ' Othello: $500.00 (40 seats)\n' +
+        'Amount owed is $1,730.00\n' +
+        'You earned 47 credits\n';
     expect(statement(invoice, plays)).toBe(expected);
 });
