06abaeae204ff0179da47271420ea027da360865
Yuya Kazama <yuya.kazama@bizreach.co.jp>
2020-09-29 15:39:56 +0900

    switch文にあるtragedy_invoiceのテストケースを作成する

--- javascript/test/statement.test.js
+++ javascript/test/statement.test.js
@@ -12,3 +12,15 @@ test('example statement', () => {
         'You earned 47 credits\n';
     expect(statement(invoice, plays)).toBe(expected);
 });
+
+test('Tragedyのタイプで観客数が31人以上と30人以下の場合の検証', () => {
+    const invoice = JSON.parse(fs.readFileSync('test/tragedy_invoice.json', 'utf8'));
+    const plays = JSON.parse(fs.readFileSync('test/tragedy_plays.json', 'utf8'));
+    const expected = 'Statement for Tragedy Test\n' +
+        ' Hamlet: $400.00 (29 seats)\n' +
+        ' Hamlet2: $400.00 (30 seats)\n' +
+        ' Othello: $410.00 (31 seats)\n' +
+        'Amount owed is $1,210.00\n' +
+        'You earned 1 credits\n';
+    expect(statement(invoice, plays)).toBe(expected);
+});
