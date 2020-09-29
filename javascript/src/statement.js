6092f53723119178d1a11d32c192a9b2c61c5a99
Yuya Kazama <yuya.kazama@bizreach.co.jp>
2020-09-29 23:11:26 +0900

    invoicesデータは引数として不要になったので削除する

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -4,10 +4,10 @@ function statement (invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances;
-    return renderPlainText(statementData, invoice, plays);
+    return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
     let result = `Statement for ${data.customer}\n`;
     const format = new Intl.NumberFormat("en-US",
         {
