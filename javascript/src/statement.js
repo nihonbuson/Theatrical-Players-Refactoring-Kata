045ba47b78e81d1b7fcedf731062538890fa22a1
Yuya Kazama <yuya.kazama@bizreach.co.jp>
2020-10-07 18:53:31 +0900

    functionのスコープを変更

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -23,27 +23,28 @@ function renderPlainText(data) {
 }
 
 function htmlStatement(invoice, plays){
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
     const format = new Intl.NumberFormat("en-US",
         {
             style: "currency", currency: "USD",
             minimumFractionDigits: 2
         }).format;
-    function renderHtml(data) {
-        let result = `<h1>Statement for ${data.customer}<h1>\n`;
-        result += "<table>\n";
-        result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
-        for (let perf of data.performances){
-            result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
-            result += `<td>${format(perf.amount / 100)} </td></tr>`;
-        }
-        result += "</table>\n";
-        result += `<p>Amount owed is <em>${format(data.totalAmount)}</em></p>\n`;
-        result += `<p>You earned <em>${data.totalCredit}</em> credits</p>\n`;
-        return result;
+    let result = `<h1>Statement for ${data.customer}<h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+    for (let perf of data.performances){
+        result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${format(perf.amount / 100)} </td></tr>`;
     }
-
-    return renderHtml(createStatementData(invoice, plays));
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${format(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalCredit}</em> credits</p>\n`;
+    return result;
 }
+
 
 module.exports = {
     statement : statement,
