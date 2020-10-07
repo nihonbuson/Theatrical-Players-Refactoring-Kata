82e804ad59842a0ab1589dd266aa7aff7701180b
Yuya Kazama <yuya.kazama@bizreach.co.jp>
2020-10-07 18:57:05 +0900

    共通ロジックをfunction化する

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -14,10 +14,10 @@ function renderPlainText(data) {
         }).format;
     for (let perf of data.performances) {
         // print line for this order
-        result += ` ${perf.play.name}: ${format(perf.amount / 100)} (${perf.audience} seats)\n`;
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
     }
 
-    result += `Amount owed is ${format(data.totalAmount / 100)}\n`;
+    result += `Amount owed is ${usd(data.totalAmount)}\n`;
     result += `You earned ${data.totalCredit} credits\n`;
     return result;
 }
@@ -37,12 +37,20 @@ function renderHtml(data) {
     result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
     for (let perf of data.performances){
         result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
-        result += `<td>${format(perf.amount / 100)} </td></tr>`;
+        result += `<td>${usd(perf.amount)} </td></tr>`;
     }
     result += "</table>\n";
-    result += `<p>Amount owed is <em>${format(data.totalAmount)}</em></p>\n`;
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
     result += `<p>You earned <em>${data.totalCredit}</em> credits</p>\n`;
     return result;
+}
+
+function usd(aNumber){
+    return new Intl.NumberFormat("en-US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber/100);
 }
 
 
