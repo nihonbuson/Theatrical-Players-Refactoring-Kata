1ce7dc7a8f2e76a6af8b4d5d36f4ad5b8833ca0c
Yuya Kazama <yuya.kazama@bizreach.co.jp>
2020-10-07 18:57:30 +0900

    テスト内容が間違っていたので修正

--- javascript/test/statement.test.js
+++ javascript/test/statement.test.js
@@ -44,7 +44,7 @@ test('example htmlStatement', () => {
     const expected = '<h1>Statement for BigCo<h1>\n' +
         '<table>\n' +
         '<tr><th>play</th><th>seats</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>$650.00 </td></tr>  <tr><td>As You Like It</td><td>35</td><td>$580.00 </td></tr>  <tr><td>Othello</td><td>40</td><td>$500.00 </td></tr></table>\n' +
-        '<p>Amount owed is <em>$173,000.00</em></p>\n' +
+        '<p>Amount owed is <em>$1,730.00</em></p>\n' +
         '<p>You earned <em>47</em> credits</p>\n';
     expect(statement.htmlStatement(invoice, plays)).toBe(expected);
 });
